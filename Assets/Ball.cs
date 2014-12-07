76b293dd203581828be5c9ac6a0e25fa49c5c589
Will <wtk1138@gmail.com>
2014-12-07 14:05:25 -0900

    Adding files to new test repo

---
+++ Assets/Ball.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public bool touchingGround; 
+	public float sphereRadius = 0.5f;
+
+	public Vector3 bounceVelocity = new Vector3 ( 0, 10, 0 );
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		touchingGround = IsTouching();
+		Debug.Log (IsTouching () );
+
+		if (touchingGround == true ) {
+			Bounce ( bounceVelocity  );
+		}
+	}
+  void Function1  () {
+		Debug.Log ("This is a function");
+		float tempVar = 1.1f;
+		tempVar = 5.4f; 
+
+
+	}
+
+	void Bounce ( Vector3 bounceVelocity ) {
+		rigidbody.velocity = bounceVelocity;
+	}
+
+	bool IsTouching () {
+		return Physics.Raycast (transform.position, Vector3.down, sphereRadius );
+	}	
+}
---
+++ Assets/Syntax.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax : MonoBehaviour {
+
+	public bool boolean1 = false; 
+	private bool boolean2 = true; 
+
+	public int interger = 1; 
+	public float floatingPoint = 1.1f; 
+
+	// Use this for initialization
+	void Start () {
+	    if ( boolean1 == true) {
+		     Debug.Log ( "Awesome" );
+		}
+
+		interger = 10; 
+		floatingPoint = 5.4f; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		interger += 1;
+	}
+}
